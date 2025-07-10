2600342bf637e1b795e79117e993322d48fdf1e8
Eric Ishekwene <ericishcollege@gmail.com>
2025-07-10 16:04:25 +0200

    Image Card

--- src/components/HaircutExample.jsx
+++ src/components/HaircutExample.jsx
@@ -1,36 +1,19 @@
 import React from 'react'
-import square from '../assets/blacksquare.png'
+import image1 from '../assets/images.jpeg'
 
 function HaircutExample() {
     return (
-        <div className="flex flex-col items-center justify-center py-20 px-10 bg-gray-100">
-            <div className="w-full  flex flex-wrap flex-row items-center justify-center  bg-black">
+        <div className="flex flex-col items-center justify-center py-5 px-5 sm:px-10 md:px-16 sm:py-10 md:py-20   ">
+            <div className="w-full  flex flex-wrap flex-row items-center justify-center  bg-gray-100">
                 {/* Single Card */}
-                <div className="flex flex-col items-center justify-center  min-w-[170px] max-w-[500px] w-1/2 p-4">
-                    <div className="bg-white rounded-xl shadow-lg overflow-hidden border border-gray-200 w-full  aspect-[3/4]">
-                        <img
-                            src={square}
-                            alt="Haircut Example"
-                            className="object-cover w-full h-full"
-                        />  
-                    </div>
-                </div>
-                <div className="flex flex-col items-center justify-center  min-w-[170px] max-w-[500px] w-1/2 p-4">
-                    <div className="bg-white rounded-xl shadow-lg overflow-hidden border border-gray-200 w-full  aspect-[3/4]">
-                        <img
-                            src={square}
-                            alt="Haircut Example"
-                            className="object-cover w-full h-full"
-                        />
-                    </div>
-                </div>
-                <div className="flex flex-col items-center justify-center  min-w-[170px] max-w-[500px] w-1/2 p-4">
-                    <div className="bg-white rounded-xl shadow-lg overflow-hidden border border-gray-200 w-full  aspect-[3/4]">
-                        <img
-                            src={square}
-                            alt="Haircut Example"
-                            className="object-cover w-full h-full"
-                        />
+                <div className="flex flex-col items-center justify-center min-w-[160px] max-w-[500px] w-1/2 p-4">
+                    <div
+                        className="relative rounded-xl shadow-lg overflow-hidden border border-gray-200 w-full aspect-[3/4] bg-cover bg-center"
+                        style={{ backgroundImage: `url(${image1})` }}
+                    >
+                        <div className="absolute bottom-5 left-5 text-white text-lg font-semibold px-2 py-1 rounded">
+                            <p className="text-4xl montserrat-bold">low taper</p>
+                        </div>
                     </div>
                 </div>
 
--- src/components/subtext.jsx
+++ src/components/subtext.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function subtext({text}) {
   return (
-    <div className="flex flex-col items-center justify-center gap-10 mt-10 px-6 sm:px-10 md:px-16">
+    <div className="flex flex-col items-center justify-center py-5 px-5 sm:px-10 md:px-16 sm:py-10 md:py-20">
           <p className="birthstone-regular text-center text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl text-black max-w-4xl mx-auto">
           "{text}"
           </p>
